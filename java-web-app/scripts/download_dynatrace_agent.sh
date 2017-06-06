88860afc3a9e0e9f151480dc13e9f025f328ddf7
Josef Salyer <jsalyer@cas.org>
2017-06-06 11:05:07 -0400

    added dynatrace agent download

---
+++ java-web-app/scripts/download_dynatrace_agent.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+VERSION=6.3.0
+BUILD=1305
+
+curl -O https://files.dynatrace.com/downloads/OnPrem/dynaTrace/${VERSION}/${VERSION}.${BUILD}/dynatrace-agent-${VERSION}.${BUILD}-unix.jar
